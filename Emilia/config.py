3ea48ba32a93f88f693733977e5d58e8c70ff127
harkirat970 <139225555+harkirat970@users.noreply.github.com>
2025-07-10 03:29:41 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -8,23 +8,23 @@ def get_user_list(config, key):
 
 
 class Config(object):
-    API_HASH = "45aabfac" # API_HASH from my.telegram.org
-    API_ID = 62 # API_ID from my.telegram.org
+    API_HASH = "9bdff4a34dc5e155d1240b35a0013982" # API_HASH from my.telegram.org
+    API_ID = 20500424 # API_ID from my.telegram.org
 
     BOT_ID = 521 # BOT_ID
-    BOT_USERNAME = "Elf_Robot" # BOT_USERNAME
+    BOT_USERNAME = "Emilia49bot" # BOT_USERNAME
 
-    MONGO_DB_URL = "mongodb://username:password@localhost:27017/emi?directConnection=true&authSource=admin" # MongoDB URL from MongoDB Atlas
+    MONGO_DB_URL = "mongodb+srv://manseerat685:Kirat2006Harkirat@cluster0.fmlz30j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" # MongoDB URL from MongoDB Atlas
 
-    SUPPORT_CHAT = "SpiralTechDivision" # Support Chat Username
-    UPDATE_CHANNEL = "SpiralUpdates" # Update Channel Username
+    SUPPORT_CHAT = "deadgroupchat2" # Support Chat Username
+    UPDATE_CHANNEL = "detergentlab" # Update Channel Username
     START_PIC = "https://pic-bstarstatic.akamaized.net/ugc/9e98b6c8872450f3e8b19e0d0aca02deff02981f.jpg@1200w_630h_1e_1c_1f.webp" # Start Image
-    DEV_USERS = [6040984893, 6461051572, 7107018652] # Dev Users
-    TOKEN = "57375" # Bot Token from @BotFather
-    CLONE_LIMIT = 50 # Number of clones your bot can make
+    DEV_USERS = [7489809533] # Dev Users
+    TOKEN = "8150036125:AAE-NAbntZU5h-c4sJesPocJkKzjGqakklI" # Bot Token from @BotFather
+    CLONE_LIMIT = 0 # Number of clones your bot can make
 
-    EVENT_LOGS = -10093 # Event Logs Chat ID
-    OWNER_ID = 6040984893 # Owner ID
+    EVENT_LOGS = -1002344972842 # Event Logs Chat ID
+    OWNER_ID = 7684887614 # Owner ID
  
     TEMP_DOWNLOAD_DIRECTORY = "./" # Temporary Download Directory
     BOT_NAME = "Emilia" # Bot Name
